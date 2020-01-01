56de06d068e75353ab02a4f70445cff374292b29
Chirag Bharadwaj <bchira@amazon.com>
2020-01-01 12:07:05 -0500

    Created frame outline for quick chat plugin

--- src/main/java/com/quickchat/QuickChatPlugin.java
+++ src/main/java/com/quickchat/QuickChatPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.quickchat;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,26 +14,26 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Quick Chat"
 )
-public class ExamplePlugin extends Plugin
+public class QuickChatPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private QuickChatConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Quick Chat started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Quick Chat stopped!");
 	}
 
 	@Subscribe
@@ -41,13 +41,13 @@ public class ExamplePlugin extends Plugin
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Quick Chat says " + config.greeting(), null);
 		}
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	QuickChatConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(QuickChatConfig.class);
 	}
 }
